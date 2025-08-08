7447162d20fdaa4a227539cf75b990932d8d29a4
Muhammad Azan <azanjokhio54@gmail.com>
2025-08-08 13:31:25 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";// Treat all JS code as newer version
+
+//alert("Hello")// We are using node.js not browser
+
+// console.log(3+3);
+// console.log("Hello");
+
+let name = "Azan"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof "john"); // String
+console.log(typeof null);// Object
+console.log(typeof undefined);// Undefined
+
+
+
