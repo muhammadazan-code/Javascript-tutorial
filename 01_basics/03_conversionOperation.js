92c8eaf1ad3f51a2790b6c5e829701f7ecec67dc
Muhammad Azan <azanjokhio54@gmail.com>
2025-08-08 14:07:28 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,13 +17,13 @@ let booleanIsLoggedIn = Number(isLogged)
 
 let emptyCharacter = ""
 let convertAndCheck = Boolean(emptyCharacter)
-console.log(convertAndCheck)//false
-console.log(typeof convertAndCheck);// Boolean
+// console.log(convertAndCheck)//false
+// console.log(typeof convertAndCheck);// Boolean
 
-let a = "adfjaslfj";
-let check  = Boolean(a)
-console.log(check);//true
-console.log(typeof check);// Boolean
+// let a = "adfjaslfj";
+// let check  = Boolean(a)
+// console.log(check);//true
+// console.log(typeof check);// Boolean
 
 
 
@@ -34,3 +34,35 @@ console.log(typeof check);// Boolean
 // 1 => in Boolean true and 0 => false
 // "" => false
 // "abc" => true
+
+// *********************** Operations ******************
+
+// console.log(2+2)
+// console.log(2-12)
+// console.log(3*4)
+// console.log( 4/2)
+// console.log(3 % 5)
+// console.log(3**3)
+
+let str1 = "Hello "
+let str2 = "world"
+// console.log(str1 + str2); Hello World
+
+// console.log(1 + "2") // 12
+// console.log(1 + 2 + 4 +"3");// 73
+// console.log("1" + 2 + 5 );// 125
+
+
+// console.log(+true); 1
+// console.log(+false); 0
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+let gameCounter = 20
+// gameCounter++ 21
+++gameCounter//21 
+console.log(gameCounter);
+
+
+
+
+
