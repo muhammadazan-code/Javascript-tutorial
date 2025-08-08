7af60f1f29d955efb44434e3b7a27f5e2e14aff3
Muhammad Azan <azanjokhio54@gmail.com>
2025-08-08 14:19:45 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log("2" > 3);
+// console.log(3 > "1");
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log("Undefined"+undefined == 0);
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
